1ae2165325a9871ee45086720835c70ea0a388c4
Sam Bumgardner <Sambumgardner1@gmail.com>
2022-03-05 20:47:24 -0600

    Gave player greater control.
    Also added feature to return player to center-screen when they go off-screen. Handy for testing, and sorta functions as a gameplay feature as a low-tech reset.

--- source/player/Player.hx
+++ source/player/Player.hx
@@ -5,19 +5,45 @@ import flixel.FlxSprite;
 
 class Player extends FlxSprite
 {
+	public static var SPEEDS(default, never):Array<Int> = [0, 50, 100];
+
 	public function new(X:Float = 0, Y:Float = 0)
 	{
 		super(X, Y);
 
-		acceleration.y = 150;
+		acceleration.y = 300;
 	}
 
 	override public function update(elapsed:Float)
 	{
-		if (FlxG.keys.justPressed.W || FlxG.keys.justPressed.UP)
+		setSpeed();
+		jump();
+
+		if (!isOnScreen())
+		{
+			kill();
+		}
+		super.update(elapsed);
+	}
+
+	private function setSpeed()
+	{
+		var speedSelector = 1;
+		FlxG.keys.pressed.LEFT ? speedSelector-- : null;
+		FlxG.keys.pressed.RIGHT ? speedSelector++ : null;
+		velocity.x = SPEEDS[speedSelector];
+	}
+
+	private function jump()
+	{
+		if (FlxG.keys.justPressed.UP)
 		{
 			velocity.y = -200;
 		}
-		super.update(elapsed);
+	}
+
+	override function kill()
+	{
+		reset(FlxG.width / 2, FlxG.height / 2);
 	}
 }
