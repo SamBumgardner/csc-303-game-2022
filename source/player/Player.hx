d49cd48c5d353b8921475b7cc79227eef53a03e8
Jessica Dean <jrd2017@live.missouristate.edu>
2022-03-23 18:33:52 -0500

    added player graphic

--- source/player/Player.hx
+++ source/player/Player.hx
@@ -3,50 +3,42 @@ package player;
 import flixel.FlxG;
 import flixel.FlxSprite;
 
-
-//Add Player Visual, one size
-
-class Player extends FlxSprite
-{
+// Add Player Visual, one size
+class Player extends FlxSprite {
 	public static var SPEEDS(default, never):Array<Int> = [0, 50, 100];
 
-	public function new(X:Float = 0, Y:Float = 0)
-	{
+	public function new(X:Float = 0, Y:Float = 0) {
 		super(X, Y);
 
 		acceleration.y = 300;
+
+		this.loadGraphic(AssetPaths.playerGraphic__jpg, false, 40, 20, false, "Player");
 	}
 
-	override public function update(elapsed:Float)
-	{
+	override public function update(elapsed:Float) {
 		setSpeed();
 		jump();
 
-		if (!isOnScreen())
-		{
+		if (!isOnScreen()) {
 			kill();
 		}
 		super.update(elapsed);
 	}
 
-	private function setSpeed()
-	{
+	private function setSpeed() {
 		var speedSelector = 1;
 		FlxG.keys.pressed.LEFT ? speedSelector-- : null;
 		FlxG.keys.pressed.RIGHT ? speedSelector++ : null;
 		velocity.x = SPEEDS[speedSelector];
 	}
 
-	private function jump()
-	{
-		if (FlxG.keys.justPressed.UP)
-		{
+	private function jump() {
+		if (FlxG.keys.justPressed.UP) {
 			velocity.y = -200;
 		}
 	}
 
-	override function kill()
-	{
+	override function kill() {
 		reset(FlxG.width / 2, FlxG.height / 2);
 	}
 }
