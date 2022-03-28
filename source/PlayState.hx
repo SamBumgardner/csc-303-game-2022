67e01c3631129a2c4993a58cdcfb8127f41e90dc
Hgaddy <holdengaddy@gmail.com>
2022-03-28 17:43:53 -0500

    Revert "Initial implementation of FlyingEnemy class and its behavior."
    This reverts commit 254767ac4fcc5cbfceb0835211af8b3f5ed29b8b.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,7 +4,6 @@ import flixel.FlxG;
 import flixel.FlxState;
 import flixel.group.FlxGroup;
 import obstacle.DeadlyObstacle;
-import obstacle.FlyingEnemy;
 import obstacle.Obstacle;
 import obstacle.ObstacleGenerator;
 import player.Player;
@@ -15,11 +14,9 @@ class PlayState extends FlxState
 
 	var obstacleGenerator:ObstacleGenerator<Obstacle>;
 	var deadlyObstacleGenerator:ObstacleGenerator<DeadlyObstacle>;
-	var flyingEnemyGenerator:ObstacleGenerator<FlyingEnemy>;
 
 	var SECONDS_PER_OBSTACLE(default, never):Float = .5;
 	var SECONDS_PER_DEADLY_OBSTACLE(default, never):Float = 2;
-	var SECONDS_PER_FLYING_ENEMY(default, never):Float = 15;
 
 	override public function create()
 	{
@@ -30,7 +27,6 @@ class PlayState extends FlxState
 
 		setUpObstacles();
 		setUpDeadlyObstacles();
-		setUpFlyingEnemies();
 	}
 
 	private function setUpObstacles()
@@ -70,28 +66,11 @@ class PlayState extends FlxState
 		add(deadlyObstacleGenerator.obstacles);
 	}
 
-	private function setUpFlyingEnemies()
-	{
-		var generatedFlyingEnemies = new FlxTypedGroup<FlyingEnemy>();
-		
-		var flyingEnemy = new FlyingEnemy();
-		flyingEnemy.kill();
-		generatedFlyingEnemies.add(flyingEnemy);
-
-		var baseFlyingEnemyParameters = new ObstacleParameters(FlxG.width, FlxG.height, 50, 50, 50);
-		var flyingEnemyVariation = new ObstacleVariation(-1, 1);
-
-		flyingEnemyGenerator = new ObstacleGenerator<FlyingEnemy>(SECONDS_PER_FLYING_ENEMY, 
-			baseFlyingEnemyParameters, flyingEnemyVariation, generatedFlyingEnemies);
-		add(flyingEnemyGenerator.obstacles);
-	}
-
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
 
 		FlxG.collide(player, obstacleGenerator.obstacles);
 		FlxG.overlap(player, deadlyObstacleGenerator.obstacles, DeadlyObstacle.overlapsWithPlayer);
-		FlxG.overlap(player, flyingEnemyGenerator.obstacles, FlyingEnemy.overlapsWithPlayer);
 	}
 }
